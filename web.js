1738a1df045a270a42c877f056ee16e20cce2510
ubi-wan <mucinisli@gmail.com>
2014-05-12 17:16:22 +0000

    update index.html
    OB

--- web.js
+++ web.js
@@ -1,15 +1,17 @@
 var express = require('express');
-var app = express();
-app.use(express.logger());
+var fs = require('fs');
+var htmlfile = "index.html";
+
+var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
     // response.send('Hello World!2');
-	var buffer = New Buffer(50);
-	buffer = fs.readFileySync(index.html);
-	console.log(buffer.toString(''utf8,0,len));
+	var html = fs.readFileSync(htmlfile).toString();
+	response.send(html);
+	
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
